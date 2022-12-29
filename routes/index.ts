ff09a909b6cb4135ce16e0ea5eae6a9407745eaa
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-12-29 00:59:51 -0300

    Get, Post,Put, Delete and get by ID

--- routes/index.ts
+++ routes/index.ts
@@ -3,29 +3,41 @@ import Clothe from "../models/clothe";
 
 const router = Router();
 
-router.get("/clothes", async (req,res) => {
-  const clothes = await Clothe.find()
-  res.send(clothes)
-})
+router.get("/clothes", async (req, res) => {
+  const clothes = await Clothe.find();
+  res.send(clothes);
+});
 router.post("/clothes", async (req, res) => {
-  const {name, description, size, price} = req.body
+  const { name, description, size, price } = req.body;
   const clothe = new Clothe({ name, description, size, price });
-  await clothe.save()
-  res.json(clothe)
+  await clothe.save();
+  res.json(clothe);
 });
 router.get("/clothes/:id", async (req, res) => {
   try {
     const clothe = await Clothe.findById(req.params.id);
+
+    if (!clothe) return res.status(400).json({ message: "Task not found" });
+
     res.send(clothe);
   } catch (error) {
-    console.log(error)
+    return res.status(500).send(error);
   }
 });
-router.delete("/clothes/:id", (req, res) => {
-  res.send("Deleting a clothe");
+router.delete("/clothes/:id", async (req, res) => {
+  try {
+    const clothe = await Clothe.findByIdAndDelete(req.params.id);
+
+    if (!clothe) return res.status(400).json({ message: "Task not found" });
+
+    return res.json(clothe);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
-router.put("/clothes/:id", (req, res) => {
-  res.send("Updating a clothe");
+router.put("/clothes/:id", async (req, res) => {
+ const updatedClothe = await Clothe.findByIdAndUpdate(req.params.id, req.body, {new:true})
+ res.json(updatedClothe)
 });
 
-export default router
+export default router;
